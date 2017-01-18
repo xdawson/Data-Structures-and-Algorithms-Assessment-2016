ddf4cbf6d5d4af942c71c3303582f16d93e0d190
mkDoodle <chrisdawson1996@gmail.com>
2017-01-18 00:09:41 +0000

    Started implementing the predictive text functionality

--- Pedictive Tect Assessment/src/main.c
+++ Pedictive Tect Assessment/src/main.c
@@ -5,12 +5,13 @@
 trie_node*
 CalculateNextNode(trie_node* CurrentNode)
 {
-	  
+
 }
 
 
 //might need to move this to a Trie file to do data hiding things?
-trie_root_node* 
+//TODO split this up into smaller functions and comment the shit out of this mess
+trie_root_node*
 CreateTrie(char* TextFile)
 {
 	trie_root_node *Root = TrieRootNodeConstructor();
@@ -23,12 +24,12 @@ CreateTrie(char* TextFile)
 	{
 		//the \r case will already have reset the CurrentNode to Root
 		//so here we can just skip \n
-		if(TextFile[Position] == '\n')
+		if((TextFile[Position] == '\r') || (TextFile[Position] == '\n'))
 		{
 			continue;
 		}
 
-		//This is here because all the weird characters 
+		//This is here because all the weird characters
 		//at the end of the text file have values of < 0
 		//NEED TO TEST THIS IN LABSSS
 		if(TextFile[Position] < 0)
@@ -40,7 +41,24 @@ CreateTrie(char* TextFile)
 		{
 			//useful here as checks for repeated words
 			//check
-			
+			if(TextFile[Position + 1] == '\r')
+			{
+				CurrentNode->WordMarker = 1;
+				CurrentNode = Root->FirstLetter;
+			}
+			else
+			{
+				//deal with it
+				if(CurrentNode->Child != NULL)
+				{
+					CurrentNode = CurrentNode->Child;
+				}
+				else
+				{
+					CurrentNode->Child = TrieNodeConstructor(0);
+					CurrentNode = CurrentNode->Child;
+				}
+			}
 		}
 		else
 		{
@@ -49,7 +67,7 @@ CreateTrie(char* TextFile)
 				CurrentNode->Value = TextFile[Position];
 
 				//check
-				if(TextFile[Position] == '\r')
+				if(TextFile[Position + 1] == '\r')
 				{
 					CurrentNode->WordMarker = 1;
 					CurrentNode = Root->FirstLetter;
@@ -59,7 +77,7 @@ CreateTrie(char* TextFile)
 					//deal with it
 					if(CurrentNode->Child != NULL)
 					{
-						CurrentNode = CurrentNode->Child;		
+						CurrentNode = CurrentNode->Child;
 					}
 					else
 					{
@@ -75,7 +93,7 @@ CreateTrie(char* TextFile)
 					if(CurrentNode->Value == TextFile[Position])
 					{
 						//check
-						if(TextFile[Position] == '\r')
+						if(TextFile[Position + 1] == '\r')
 						{
 							CurrentNode->WordMarker = 1;
 							CurrentNode = Root->FirstLetter;
@@ -85,7 +103,7 @@ CreateTrie(char* TextFile)
 							//deal with it
 							if(CurrentNode->Child != NULL)
 							{
-								CurrentNode = CurrentNode->Child;		
+								CurrentNode = CurrentNode->Child;
 							}
 							else
 							{
@@ -101,26 +119,46 @@ CreateTrie(char* TextFile)
 					}
 				}
 
-				//dealing with case of the last link with Sibling being Null but this being the right 
+				//dealing with case of the last link with Sibling being Null but this being the right
 				//value
 				if(CurrentNode->Value == TextFile[Position])
 				{
-					if(CurrentNode->Child != NULL)
+					//check
+					if(TextFile[Position + 1] == '\r')
 					{
-						CurrentNode = CurrentNode->Child;		
+						CurrentNode->WordMarker = 1;
+						CurrentNode = Root->FirstLetter;
+					}
+					else
+					{
+						//deal with it
+						if(CurrentNode->Child != NULL)
+						{
+							CurrentNode = CurrentNode->Child;
+						}
+						else
+						{
+							CurrentNode->Child = TrieNodeConstructor(0);
+							CurrentNode = CurrentNode->Child;
+						}
+					}
+				}
+				else
+				{
+					CurrentNode->Sibling = TrieNodeConstructor(TextFile[Position]);
+					CurrentNode = CurrentNode->Sibling;
+
+					//check
+					if(TextFile[Position + 1] == '\r')
+					{
+						CurrentNode->WordMarker = 1;
+						CurrentNode = Root->FirstLetter;
 					}
 					else
 					{
 						CurrentNode->Child = TrieNodeConstructor(0);
 						CurrentNode = CurrentNode->Child;
 					}
-				}
-				else if(CurrentNode->Sibling == NULL)
-				{
-					CurrentNode->Sibling = TrieNodeConstructor(TextFile[Position]);
-					CurrentNode = CurrentNode->Sibling;
-					CurrentNode->Child = TrieNodeConstructor(0);
-					CurrentNode = CurrentNode->Child;
 				}
 			}
 		}
@@ -137,6 +175,52 @@ main()
 	char *TextFile = (char *)FileMemory;
 
 	trie_root_node* Root = CreateTrie(TextFile);
+
+	int KeyPressed;
+
+	//ASCII Keycode for ESC
+	int EscapeKey = 27;
+
+	int SpaceKey = 32;
+
+	int TabKey = 9;
+
+	//10 here is a starting estimate of how long the largest work in the 
+	//dictionary is. Probably good to find the size of the largest word when poplating the trie
+	int BufferSize = 10;
+	int *PrefixBuffer = malloc(sizeof(int) * BufferSize);
+
+	int LetterCounter = 0;
+
+	//text entry loop
+	while(KeyPressed != EscapeKey)
+	{
+		KeyPressed = getch();
+
+		if(KeyPressed == SpaceKey)
+		{
+			//clear the prefix buffer
+			int i;
+			for(i = 0; i < BufferSize; i++)
+			{
+				PrefixBuffer[i] = 0;
+			}
+
+			LetterCounter = 0;
+		}
+		else if(KeyPressed == TabKey)
+		{
+			//give suggestions
+			
+		}
+		else
+		{
+			PrefixBuffer[LetterCounter++] = KeyPressed;
+		}
+
+
+
+	}
 
 
 
