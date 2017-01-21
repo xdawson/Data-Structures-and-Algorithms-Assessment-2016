f1324e060379697a9b840f495d7d87b4d95e6616
Chris <chrisdawson1996@gmail.com>
2017-01-21 15:13:51 +0000

    Finished first pass on the program

--- Pedictive Tect Assessment/src/main.c
+++ Pedictive Tect Assessment/src/main.c
@@ -5,6 +5,54 @@
 #include <conio.h>
 #include <string.h>
 
+int 
+NumberOfSiblings(trie_node *CurrentNode)
+{
+	trie_node *Temp = CurrentNode;
+
+	int NumberOfSiblings = 0;
+
+	while(Temp->Sibling != NULL)
+	{
+		Temp = Temp->Sibling;
+		NumberOfSiblings++;
+	}
+
+	return NumberOfSiblings;
+}
+
+trie_node *
+MoveToChild(trie_node *CurrentNode)
+{
+	if(CurrentNode->Child != NULL)
+	{
+		CurrentNode = CurrentNode->Child;
+	}
+	else
+	{
+		CurrentNode->Child = TrieNodeConstructor(0);
+		CurrentNode = CurrentNode->Child;
+	}
+
+	return CurrentNode;
+}
+
+trie_node *
+FindNextNode(trie_root_node *Root, trie_node *CurrentNode, char *TextFile, int Position)
+{
+	if(TextFile[Position + 1] == '\r')
+	{
+		CurrentNode->WordMarker = 1;
+		CurrentNode = Root->FirstLetter;
+	}
+	else
+	{
+		CurrentNode = MoveToChild(CurrentNode);
+	}
+
+	return CurrentNode;
+}
+
 
 //might need to move this to a Trie file to do data hiding things?
 //TODO split this up into smaller functions and comment the shit out of this mess
@@ -37,131 +85,111 @@ CreateTrie(char* TextFile)
 		if(TextFile[Position] == CurrentNode->Value)
 		{
 			//useful here as checks for repeated words
-			//check
-			if(TextFile[Position + 1] == '\r')
-			{
-				CurrentNode->WordMarker = 1;
-				CurrentNode = Root->FirstLetter;
-			}
-			else
-			{
-				//deal with it
-				if(CurrentNode->Child != NULL)
-				{
-					CurrentNode = CurrentNode->Child;
-				}
-				else
-				{
-					CurrentNode->Child = TrieNodeConstructor(0);
-					CurrentNode = CurrentNode->Child;
-				}
-			}
+			CurrentNode = FindNextNode(Root, CurrentNode, TextFile, Position);
 		}
 		else
 		{
 			if(CurrentNode->Value == 0)
 			{
 				CurrentNode->Value = TextFile[Position];
-
-				//check
-				if(TextFile[Position + 1] == '\r')
-				{
-					CurrentNode->WordMarker = 1;
-					CurrentNode = Root->FirstLetter;
-				}
-				else
-				{
-					//deal with it
-					if(CurrentNode->Child != NULL)
-					{
-						CurrentNode = CurrentNode->Child;
-					}
-					else
-					{
-						CurrentNode->Child = TrieNodeConstructor(0);
-						CurrentNode = CurrentNode->Child;
-					}
-				}
+				CurrentNode = FindNextNode(Root, CurrentNode, TextFile, Position);
 			}
 			else
 			{
-				while(CurrentNode->Sibling != NULL)
+				int i;
+				int NumSiblings = NumberOfSiblings(CurrentNode);
+
+				for(i = 0; i <= NumSiblings; i++)
 				{
 					if(CurrentNode->Value == TextFile[Position])
 					{
-						//check
-						if(TextFile[Position + 1] == '\r')
-						{
-							CurrentNode->WordMarker = 1;
-							CurrentNode = Root->FirstLetter;
-						}
-						else
-						{
-							//deal with it
-							if(CurrentNode->Child != NULL)
-							{
-								CurrentNode = CurrentNode->Child;
-							}
-							else
-							{
-								CurrentNode->Child = TrieNodeConstructor(0);
-								CurrentNode = CurrentNode->Child;
-							}
-						}
+						CurrentNode = FindNextNode(Root, CurrentNode, TextFile, Position);
 						break;
 					}
 					else
 					{
-						CurrentNode = CurrentNode->Sibling;
-					}
-				}
-
-				//dealing with case of the last link with Sibling being Null but this being the right
-				//value
-				if(CurrentNode->Value == TextFile[Position])
-				{
-					//check
-					if(TextFile[Position + 1] == '\r')
-					{
-						CurrentNode->WordMarker = 1;
-						CurrentNode = Root->FirstLetter;
-					}
-					else
-					{
-						//deal with it
-						if(CurrentNode->Child != NULL)
+						if(CurrentNode->Sibling == NULL)
 						{
-							CurrentNode = CurrentNode->Child;
+							CurrentNode->Sibling = TrieNodeConstructor(TextFile[Position]);
+							CurrentNode = CurrentNode->Sibling;
+
+							CurrentNode = FindNextNode(Root, CurrentNode, TextFile, Position);
 						}
 						else
 						{
-							CurrentNode->Child = TrieNodeConstructor(0);
-							CurrentNode = CurrentNode->Child;
+							CurrentNode = CurrentNode->Sibling;
 						}
 					}
+
+				}
+			}
+		}
+	}
+	return Root;
+}
+
+void
+FlushBuffer(char *Buffer, int BufferSize)
+{
+	int i;
+	for(i = 0; i < BufferSize; i++)
+	{
+		Buffer[i] = 0;
+	}
+}
+
+trie_node *
+FindPrefixNode(trie_root_node *Root, char *PrefixBuffer)
+{
+	trie_node *CurrentNode = Root->FirstLetter;
+
+	//find the position of this prefix in the trie
+	int i;
+	for(i = 0; PrefixBuffer[i] != 0; i++)
+	{
+		if(PrefixBuffer[i] == CurrentNode->Value)
+		{
+			CurrentNode = CurrentNode->Child;
+		}
+		else
+		{
+			int j;
+			int NumSiblings = NumberOfSiblings(CurrentNode);
+
+			for(j = 0; j <= NumSiblings; j++)
+			{
+				if(PrefixBuffer[i] == CurrentNode->Value)
+				{
+					CurrentNode = CurrentNode->Child;
+					break;
 				}
 				else
 				{
-					CurrentNode->Sibling = TrieNodeConstructor(TextFile[Position]);
-					CurrentNode = CurrentNode->Sibling;
-
-					//check
-					if(TextFile[Position + 1] == '\r')
+					if(CurrentNode->Sibling != NULL)
 					{
-						CurrentNode->WordMarker = 1;
-						CurrentNode = Root->FirstLetter;
+						CurrentNode = CurrentNode->Sibling;								
 					}
 					else
 					{
-						CurrentNode->Child = TrieNodeConstructor(0);
-						CurrentNode = CurrentNode->Child;
+						printf("\nNo words with current prefix\n");
+						return NULL;
 					}
 				}
 			}
 		}
 	}
+	return CurrentNode;
+}
 
-	return Root;
+int 
+NumberOfEnteredCharacters(char *Buffer)
+{
+	int Position = 0;
+	while(Buffer[Position] != 0)
+	{
+		Position++;
+	}
+	return Position;
 }
 
 int
@@ -173,7 +201,7 @@ main()
 
 	trie_root_node* Root = CreateTrie(TextFile);
 
-	char KeyPressed;
+	char KeyPressed = 0;
 
 	//ASCII Keycodes
 	int EscapeKey = 27;
@@ -182,17 +210,21 @@ main()
 
 	int TabKey = 9;
 
-	//20 here is a starting estimate of how long the largest work in the 
+	//20 here is a starting estimate of how long the largest work in the
 	//dictionary is. Probably good to find the size of the largest word when poplating the trie
 	int BufferSize = 20;
+	//A large number
+	//space for 256 characters
+	int InputBufferSize = 256;
+	int InputBufferPosition = 0;
+
+	char *InputBuffer = malloc(sizeof(char) * InputBufferSize);
 	char *PrefixBuffer = malloc(sizeof(char) * BufferSize);
 	char *OutputBuffer = malloc(sizeof(char) * BufferSize);
 	//flush buffer before use
-	int i;
-	for(i = 0; i < BufferSize; i++)
-	{
-		PrefixBuffer[i] = 0;
-	}
+	FlushBuffer(InputBuffer, InputBufferSize);
+	FlushBuffer(PrefixBuffer, BufferSize);
+	FlushBuffer(OutputBuffer, BufferSize);
 
 	int LetterCounter = 0;
 
@@ -202,95 +234,93 @@ main()
 		do
 		{
 			KeyPressed = getch();
-			printf("%c", KeyPressed);
+
+			//only test for tab?
+			if(KeyPressed != TabKey)
+			{
+				InputBuffer[InputBufferPosition++] = KeyPressed;				
+			}
 		}
 		while(KeyPressed == 0);
 
 		if(KeyPressed == SpaceKey)
 		{
 			//clear the prefix buffer
-			int i;
-			for(i = 0; i < BufferSize; i++)
-			{
-				PrefixBuffer[i] = 0;
-			}
-
+			FlushBuffer(PrefixBuffer, BufferSize);
 			LetterCounter = 0;
 		}
 		else if(KeyPressed == TabKey)
 		{
-			//give suggestions
-			trie_node *CurrentNode = Root->FirstLetter;
+			trie_node *PrefixNode = FindPrefixNode(Root, PrefixBuffer);
 
-			int FoundPrefix = 1;
-
-			//find the position of this prefix in the trie
-			int i;
-			for(i = 0; PrefixBuffer[i] != 0; i++)
-			{
-				if(PrefixBuffer[i] == CurrentNode->Value)
-				{
-					CurrentNode = CurrentNode->Child;
-				}
-				else
-				{
-					while(CurrentNode->Sibling != NULL)
-					{
-						if(CurrentNode->Value == PrefixBuffer[i])
-						{
-							CurrentNode = CurrentNode->Child;
-							break;
-						}
-						else
-						{
-							CurrentNode = CurrentNode->Sibling;
-						}
-					}
-					//continue;
-
-					//maybe dont need if as i know the control flow,
-					//will only get here if sibling == NULL
-					if(CurrentNode->Sibling == NULL)
-					{
-						if(CurrentNode->Value == PrefixBuffer[i])
-						{
-							CurrentNode = CurrentNode->Child;
-						}
-						else
-						{
-							printf("No words with current prefix\n");
-							FoundPrefix = 0;
-							//breaks out of the for loop
-							break;
-						}
-					}
-
-
-				}
-			}
-
-			//found current node
-			if(FoundPrefix)
+			if(PrefixNode)
 			{
 				strcpy(OutputBuffer, PrefixBuffer);
 
-				printf("%s", OutputBuffer);
+				trie_node *Temp = PrefixNode;				
 
-				
-				/*int i;
-				int NumberOfOptionsToDisplay = 3;
-				for(i = 0; i < NumberOfOptionsToDisplay; i++)
+				//find the first 0 char of output buffer
+				int Position = 0;
+				while(OutputBuffer[Position] != 0)
 				{
-					
-				}*/
-			}
+					Position++;
+				}
 
+				trie_node *Option;
+
+				while(Temp != NULL)
+				{
+					OutputBuffer[Position++] = Temp->Value;
+
+					if(Temp->WordMarker == 1)
+					{
+						
+						Option = Temp;
+						printf("\n1. %s", OutputBuffer);
+
+						int Chosen = 0;
+						while(!Chosen)
+						{
+							char Key = getch();
+
+							if(Key != 0)
+							{
+								//accept word
+								if(Key == '1')
+								{
+									strcpy(&InputBuffer[(InputBufferPosition - LetterCounter)], OutputBuffer);
+									InputBufferPosition = NumberOfEnteredCharacters(InputBuffer);
+
+									InputBuffer[InputBufferPosition++] = SpaceKey; 
+									FlushBuffer(PrefixBuffer, BufferSize);
+									LetterCounter = 0;
+									Chosen = 1;
+								}
+								//reject word
+								else
+								{
+									InputBuffer[InputBufferPosition++] = Key;
+									PrefixBuffer[LetterCounter++] = Key;
+									Chosen = 1;
+								}
+								
+								FlushBuffer(OutputBuffer, BufferSize);
+							}
+						}
+
+						break;
+					}
+					
+					Temp = Temp->Child;
+				}
+			}
 		}
 		else
 		{
 			PrefixBuffer[LetterCounter++] = KeyPressed;
 		}
 
+		printf("\n%s", InputBuffer);
 
 
 	}
